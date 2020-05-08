e0683ab68dad05c99ec7fdd8b588432fa27f52d2
shankygupta79 <shankygupta79@gmail.com>
2020-05-08 21:15:23 +0530

    Initial

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const express = require('express')
-const bodyParser= require('body-parser');
-const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
-//ROUTES WILL GO HERE
-const path = require('path');
 
+const express = require('express');
+const session = require('express-session')
+const SERVER_PORT=process.env.PORT || 3989
+const path=require('path')
+const app = express();
+
+app.set('views', path.join(__dirname, 'views/'));
+app.use((req, res, next) => {
+    next()
+})
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'something very very secret'
+  }))
+  
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 var uploadRouter = require('./routes/upload');
 app.use('/', uploadRouter);
-app.listen(3000, () => console.log('http://localhost:3000/'));
+
+app.listen(SERVER_PORT, function () {
+    console.log("Server started on http://localhost:3989/");
+});
+//taskkill/f /im node.exe
